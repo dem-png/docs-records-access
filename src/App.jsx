d1f75759906458a3111b0908ede31a76b677b0cd
Ken Demetri B. Payo <kendemetribanguardiapayo@gmail.com>
2026-01-27 11:40:49 +0800

    with_masterlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import Dashboard from "./documents_dashboard/dashboard.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/dashboard.jsx";
+import Masterlist from "./pages/masterlist.jsx";
+import "./index.css";
 
 export default function App() {
-  return <Dashboard />;
+  return (
+    <Routes>
+      {/* default */}
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
+      {/* pages */}
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/documents" element={<Masterlist />} />
+
+      {/* optional: if you don't have this page yet, remove the button in dashboard */}
+      {/* <Route path="/request-document" element={<RequestDocument />} /> */}
+    </Routes>
+  );
 }
