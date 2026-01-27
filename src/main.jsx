e66692164259a27fd2b9271a4fca9ca5251dbaa0
Ken Demetri B. Payo <kendemetribanguardiapayo@gmail.com>
2026-01-28 00:43:15 +0800

    merged again

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -21,4 +9,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>
 );
->>>>>>> 842fd4a1723d3116580c9f4dc09e238de7f7523e
